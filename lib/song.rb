4a5aae9c3b26b9968740849667f25fb43da3451e
Sundus <sundusaden105@gmail.com>
2023-02-14 15:53:46 +0300

    completed

---
+++ lib/song.rb
@@ -0,0 +1,47 @@
+class Song
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@artists.push(artist)
+        @@genres << genre
+        @@count += 1
+    end
+
+    def name
+        @name
+    end
+
+    def artist
+        @artist
+    end
+    def genre
+        @genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+
+end
